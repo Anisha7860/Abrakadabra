a7ea55c94da5de23b9d5423817c996dad6e29bb2
AkhileshManda <70640191+AkhileshManda@users.noreply.github.com>
2021-05-09 01:00:17 +0530

    Update script2.js

--- quiz_app_js/script2.js
+++ quiz_app_js/script2.js
@@ -1,82 +1,82 @@
 const quizDB=[
     {
-        question: "Q1 DS",
-        a:"Option1 q1",
-        b:"Option2 q1",
-        c:"Option3 q1",
-        d:"Option4 q1",
+        question: "What type of a surgeon was Dr. Strange?",
+        a:"Neuro Surgeon",
+        b:"Cardiothoracic Surgeon",
+        c:"Oculoplastic Surgeon",
+        d:"General Surgeon",
+        ans:"ans1"
+    },
+    {
+        question: "Why did Dr.Strange go to Kamar Taj?",
+        a:"To meet the Ancient One",
+        b:"To heal himself ",
+        c:"As a part of tour",
+        d:"To attend a Birthday Party",
+        ans:"ans2"
+    },
+    {
+        question: "What was the first trick Dr.Strange performed using the Eye of Agamotto?",
+        a:"Kill the Ancient One",
+        b:"Retrieve the Lost Pages of the Cagliostro Book",
+        c:"Make himself invisible",
+        d:"Used a time loop on a biten apple to restore it back",
         ans:"ans4"
     },
     {
-        question: "Q2",
-        a:"Option1",
-        b:"Option2",
-        c:"Option3",
-        d:"Option4",
+        question: "Who was Dr.Strange's teacher at Kamar Taj?",
+        a:"The Ancient One",
+        b:"Hamir",
+        c:"Kaecilius",
+        d:"Dormamu",
+        ans:"ans1"
+    },
+    {
+        question: "What was used to create portals to go from place to place?",
+        a:"Bare Hands",
+        b:"Slip Ring",
+        c:"Eye of Agamotto",
+        d:"Sling Ring",
         ans:"ans4"
     },
     {
-        question: "Q3",
-        a:"Option1",
-        b:"Option2",
-        c:"Option3",
-        d:"Option4",
+        question: "From where did the Ancient One draw her powers to become immortal?",
+        a:"From a Cafe",
+        b:"From The New York Sanctrum",
+        c:"From The Dark Dimension",
+        d:"From Kamar Taj",
+        ans:"ans3"
+    },
+    {
+        question: "'We don't lose our demons, Mordo. We only learn to live above them.' Was said by?",
+        a:"Dr.Strange",
+        b:"Dormamu",
+        c:"Kaecilius",
+        d:"The Ancient One",
         ans:"ans4"
     },
     {
-        question: "Q4",
-        a:"Option1",
-        b:"Option2",
-        c:"Option3",
-        d:"Option4",
-        ans:"ans4"
+        question: "Name the Book that Kaecilius tore pages from",
+        a:"Diary of The Ancient One",
+        b:"Book of Cagliostro",
+        c:"Road To The Dark Dimension",
+        d:"Book of Agamotto",
+        ans:"ans2"
     },
     {
-        question: "Q5",
-        a:"Option1 q1",
-        b:"Option2 q1",
-        c:"Option3 q1",
-        d:"Option4 q1",
-        ans:"ans4"
-    },
-    {
-        question: "Q6",
-        a:"Option1 q1",
-        b:"Option2 q1",
-        c:"Option3 q1",
-        d:"Option4 q1",
-        ans:"ans4"
-    },
-    {
-        question: "Q7",
-        a:"Option1 q1",
-        b:"Option2 q1",
-        c:"Option3 q1",
-        d:"Option4 q1",
-        ans:"ans4"
-    },
-    {
-        question: "Q8",
-        a:"Option1 q1",
-        b:"Option2 q1",
-        c:"Option3 q1",
-        d:"Option4 q1",
-        ans:"ans4"
-    },
-    {
-        question: "Q9",
-        a:"Option1 q1",
-        b:"Option2 q1",
-        c:"Option3 q1",
-        d:"Option4 q1",
-        ans:"ans4"
+        question: "How did Dr.Strange get Dormamu to take his troops away from Earth?",
+        a:"Using an endless Time Loop to Bargain",
+        b:"Using the most powerfull Magic Spell",
+        c:"Begged Dormamu to leave!",
+        d:"By Hypnotizing Dormamu",
+        ans:"ans1"
     }, 
     {
-        question: "Q10",
-        a:"Option1 q1",
-        b:"Option2 q1",
-        c:"Option3 q1",
-        d:"Option4 q1",
+        question: "'Just Wong? Like Adele?' Was said by?",
+        a:"Mordo",
+        b:"Chrisitine Palmer",
+        c:"Stan Lee(the Bus Driver)",
+        d:"Dr.Strange",
         ans:"ans4"
     },
     
